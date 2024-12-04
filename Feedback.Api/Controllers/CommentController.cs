7caa6149af66d9f9e977ceb924cc425983b6699f
Steijn Ploegmakers <461991@student.fontys.nl>
2024-12-04 01:28:05 +0100

    chore: changed routing in controller

--- Feedback.Api/Controllers/CommentController.cs
+++ Feedback.Api/Controllers/CommentController.cs
@@ -2,14 +2,14 @@ using Feedback.Application.Logic.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-
 namespace Feedback.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("feedback/v1/comment")]
     public class CommentController : ControllerBase
     {
         [HttpPost]
+        [Route("create")]
         public async Task<IActionResult> CreateComment([FromServices] IMediator mediator, [FromBody] CreateCommentCommand command)
         {
             await mediator.Send(command);
